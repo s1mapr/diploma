fb624e11ddba55218ce502ef050cebe7e1c7824c
Maksym Prokopenko <mpr@empat.tech>
2024-11-03 20:17:01 +0200

    small fix

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ChatController;
 use App\Http\Controllers\Api\ContentBlockController;
 use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\Api\LessonController;
